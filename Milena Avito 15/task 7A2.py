885dd17ad37a09a227e4bac75b615df7577ffd83
AndreyVeshchev <aiveshchev@gmail.com>
2024-09-20 11:25:52 +0300

    Update task 7A2.py
    divisor

--- Milena Avito 15/task 7A2.py
+++ Milena Avito 15/task 7A2.py
@@ -1,5 +1,9 @@
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
+divisor = False
 for i in range(2, min(num1, num2) + 1):
         if (num1 % i == 0) and (num2 % i == 0):
             print(i)
+            divisor = True
+if divisor == False:
+    print("Делители отсутвуют")
